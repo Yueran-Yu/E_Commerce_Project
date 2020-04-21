e44c533409116368b0b1a589b08ac42ee7258c69
patrick.jiang <patrick.jiang86@gmail.com>
2020-04-21 13:43:10 -0500

    Remove user auth to view website

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,35 +1,31 @@
-class ProductsController < ApplicationController
-  before_action :authenticate_devise_user!
+# frozen_string_literal: true
 
+class ProductsController < ApplicationController
   def index
     @categories = Category.all.includes(:products)
 
     cate = params[:cate]
     if !cate.nil?
-      @category  = Category.find(cate)
+      @category = Category.find(cate)
       @products = Product.where(category_id: cate).order(:id).page(params[:page])
     else
       @products = Product.order(:id).page(params[:page])
     end
-
   end
 
   def show
     @product = Product.find(params[:id])
     @cate = Category.find(@product.category_id)
-
   end
 
   # GET /search/?search_temr = user + search +terms
   def search
-      if params[:category_id].blank?
-          @products = Product.where("name LIKE ?", "%#{params[:search_term]}%")
-          @cate = nil
-      else
-        @products = Category.find(params[:category_id]).products.where("name LIKE ?", "%#{params[:search_term]}%")
-        @cate = Category.find(params[:category_id])
-      end
-
+    if params[:category_id].blank?
+      @products = Product.where('name LIKE ?', "%#{params[:search_term]}%")
+      @cate = nil
+    else
+      @products = Category.find(params[:category_id]).products.where('name LIKE ?', "%#{params[:search_term]}%")
+      @cate = Category.find(params[:category_id])
+    end
   end
 end
-
